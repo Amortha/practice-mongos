ed0463cb220420b95667b35dfa2e953031d2619f
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-05-12 20:33:54 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import app from "./app";
+
+const port: number = 5000
+
+// database connection
+async function bootstrap() {
+   try{
+    await mongoose.connect('mongodb://127.0.0.1:27017/practice-mongoose');
+    console.log(`Database connection successful`)
+  
+    app.listen(port, () => {
+      console.log(`server is listening on port ${port}`)
+    })
+   }
+   catch(err){
+    console.log(`Filed to connect database`, err)
+   }
+}
+
+bootstrap()
+
+
